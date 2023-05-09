cc674dac7b81afbe065e06249a1cb5b1ec1f01c1
nishant-sachdeva <nishant.sachdeva@research.iiit.ac.in>
2023-05-09 10:20:28 +0530

    added basic tests for the main run:

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,95 @@
 pub mod config;
 pub mod interpreter;
 
-pub fn run_interpreter(_config: &config::Config) -> Result<bool, String>{
-    Ok(true)
+use interpreter::{
+    evaluate::{
+        object_system,
+        environment,
+        eval
+    },
+    parser,
+    lexer,
+};
+
+use std::fs;
+
+fn run_code(input: &str) -> object_system::Object {
+    let lexer = lexer::Lexer::new(input.to_string());
+    let mut parser = parser::Parser::new(lexer);
+
+    let program = match parser.parse_program() {
+        Ok(program) => program,
+        Err(e) => {
+            return object_system::Object::EvalError(
+                object_system::EvalError{
+                    message: e.message
+                }
+            )
+        }
+    };
+
+    let mut env = environment::Environment::new(Box::new(None));
+
+    let result = eval(program, &mut env);
+    result
+}
+
+pub fn run_interpreter(config: &config::Config) -> object_system::Object {
+    // read code from config file
+    let code = fs::read_to_string(*config.file_name.clone())
+        .expect("Something went wrong reading the file");
+
+    // run code
+    let result = run_code(&code);
+    result
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_interpreter_run() {
+        struct Test {
+            input: String,
+            expected: object_system::Object,
+        }
+
+        let inputs = vec![
+            Test {
+                input: "let a = 5;".to_string(),
+                expected: object_system::Object::Null,
+            },
+            Test {
+                input: "let a = 5; a;".to_string(),
+                expected: object_system::Object::Integer(
+                    object_system::Integer { value: 5 }
+                ),
+            },
+            Test {
+                input: "let a = 5 * 5; a;".to_string(),
+                expected: object_system::Object::Integer(
+                    object_system::Integer { value: 25 }
+                ),
+            },
+            Test {
+                input: "let a = 5; let b = a; b;".to_string(),
+                expected: object_system::Object::Integer(
+                    object_system::Integer { value: 5 }
+                ),
+            },
+            Test {
+                input: "let a = 5; let b = a; let c = a + b + 5; c;".to_string(),
+                expected: object_system::Object::Integer(
+                    object_system::Integer { value: 15 }
+                ),
+            },
+        ];
+
+        for test in inputs {
+            let result = run_code(&test.input);
+            assert_eq!(result, test.expected);
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 pub use monkey_lang::{
     config::Config,
+    interpreter::evaluate,
     run_interpreter
 };
 
@@ -13,13 +14,15 @@ fn main() {
     };
 
     match run_interpreter(&config) {
-        Ok(result) => {
-            assert_eq!(result, true);
-            std::process::exit(0);
-        }
-        Err(err) => {
-            eprintln!("Error {}", err);
-            std::process::exit(1);
-        }
+        evaluate::object_system::Object::Integer(i) => println!("{:?}", i),
+        evaluate::object_system::Object::Boolean(b) => println!("{:?}", b),
+        evaluate::object_system::Object::Null => println!("null"),
+        evaluate::object_system::Object::EvalError(e) => println!("{:?}", e),
+        evaluate::object_system::Object::ReturnValue(r) => println!("{:?}", r),
+        evaluate::object_system::Object::FunctionObject(f) => println!("{:?}", f),
+        evaluate::object_system::Object::StringObject(s) => println!("{:?}", s),
+        evaluate::object_system::Object::ArrayObject(a) => println!("{:?}", a),
+        evaluate::object_system::Object::HashObject(h) => println!("{:?}", h),
+        evaluate::object_system::Object::BuiltinFunctionObject(b) => println!("{:?}", b),
     }
 }
