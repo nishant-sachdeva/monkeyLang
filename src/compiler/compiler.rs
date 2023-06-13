95bd1258bc785cb804022fbd819733df0da0193d
nishant-sachdeva <nishant.sachdeva@research.iiit.ac.in>
2023-06-13 13:40:11 +0530

    Added skeleton for compilation, raw assembly and format Assembly

---
+++ src/compiler/compiler.rs
@@ -0,0 +1,155 @@
+use crate::{
+    interpreter::evaluate::object_system::*,
+    virtual_machine::bytecode::{
+        Instructions,
+        make_bytecode,
+        OpCode
+    },
+    interpreter::ast,
+};
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct ByteCode {
+    pub instructions: Instructions,
+    pub constants: Vec<Object>,
+}
+
+pub struct Compiler {
+    pub instructions: Instructions,
+    pub constants: Vec<Object>,
+}
+
+impl Compiler {
+    pub fn new() -> Self {
+        Compiler {
+            instructions: Vec::new(),
+            constants: Vec::new(),
+        }
+    }
+
+    pub fn compile(&mut self, _program: &mut ast::Program) -> Result<(), String> {
+        return Ok(());
+    }
+
+    pub fn bytecode(&mut self) -> Result<ByteCode, String> {
+        return Ok(ByteCode {
+            instructions: Vec::new(),
+            constants: Vec::new(),
+        });
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use core::panic;
+    use std::vec;
+    use crate::{parser::Parser, interpreter::lexer::Lexer};
+
+    use super::*;
+
+    pub struct CompilerTest {
+        input: String,
+        expected_constants: Vec<Object>,
+        expected_instructions: Instructions,
+    }
+
+    pub fn test_instructions(expected: Instructions, actual: Instructions) -> Result<(), String> {
+        if expected.len() != actual.len() {
+            return Err(format!("Wrong instructions length.\nExpected: {}\nActual: {}", expected.len(), actual.len()));
+        }
+
+        for (i, expected_instruction) in expected.iter().enumerate() {
+            let actual_instruction = actual.get(i).unwrap();
+
+            if expected_instruction.len() != actual_instruction.len() {
+                return Err(format!("Wrong instruction length at {}.\nExpected: {}\nActual: {}", i, expected_instruction.len(), actual_instruction.len()));
+            }
+
+            for (j, expected_byte) in expected_instruction.iter().enumerate() {
+                let actual_byte = actual_instruction.get(j).unwrap();
+
+                if expected_byte != actual_byte {
+                    return Err(format!("Wrong byte at {}:{}.\nExpected: {}\nActual: {}", i, j, expected_byte, actual_byte));
+                }
+            }
+        }
+
+        return Ok(());
+    }
+
+    fn test_constants(expected: Vec<Object>, actual: Vec<Object>) -> Result<(), String> {
+        if expected.len() != actual.len() {
+            return Err(format!("Wrong constants length.\nExpected: {}\nActual: {}", expected.len(), actual.len()));
+        }
+
+        for (i, expected_constant) in expected.iter().enumerate() {
+            let actual_constant = actual.get(i).unwrap();
+
+            if expected_constant != actual_constant {
+                return Err(format!("Wrong constant at {}.\nExpected: {:?}\nActual: {:?}", i, expected_constant, actual_constant));
+            }
+        }
+
+        return Ok(());
+    }
+
+    pub fn parse(input: String) -> ast::Program {
+        let lexer = Lexer::new(input);
+        let mut parser = Parser::new(lexer);
+
+        return parser.parse_program().unwrap();
+    }
+
+    fn run_compiler_tests(tests: Vec<CompilerTest>) {
+        for test in tests {
+            let mut program = parse(test.input);
+
+            let mut compiler = Compiler::new();
+            let result = match compiler.compile(&mut program) {
+                Ok(_) => Ok(()),
+                Err(e) => Err(e),
+            };
+
+            assert_eq!(result, Ok(()));
+
+            let bytecode = match compiler.bytecode() {
+                Ok(bytecode) => bytecode,
+                Err(e) => panic!("{e}"),
+            };
+
+            _ = match test_instructions(test.expected_instructions, bytecode.instructions) {
+                Ok(_) => (),
+                Err(e) => panic!("{e}"),
+            };
+
+            _ = match test_constants(test.expected_constants, bytecode.constants) {
+                Ok(_) => (),
+                Err(e) => panic!("{e}"),
+            };
+        }
+    }
+
+    #[test]
+    fn test_integer_arithmetic() {
+        let input = vec![
+            CompilerTest {
+                input: String::from("1 + 2"),
+                expected_constants: vec![
+                    Object::Integer(Integer { value: 1 }),
+                    Object::Integer(Integer { value: 2 }),
+                ],
+                expected_instructions: vec![
+                    make_bytecode(OpCode::OpConstant, vec![0]).unwrap(),
+                    make_bytecode(OpCode::OpConstant, vec![1]).unwrap(),
+                ],
+            }
+        ];
+
+        run_compiler_tests(input);
+    }
+
+    // #[test]
+    // fn test_bytecode() {
+    //     unimplemented!()
+    // }
+}
--- src/virtual_machine/bytecode.rs
+++ src/virtual_machine/bytecode.rs
@@ -1,8 +1,8 @@
 use std::{vec, hash::Hash};
 
-type Byte = u8;
-type Instruction = Vec<Byte>;
-type _Instructions = Vec<Instruction>;
+pub type Byte = u8;
+pub type Instruction = Vec<Byte>;
+pub type Instructions = Vec<Instruction>;
 
 #[derive(Debug, Clone, Eq, PartialEq, Hash, Copy)]
 pub enum OpCode {
@@ -42,19 +42,19 @@ impl OpcodeLayout {
 /// # Examples
 /// 
 /// ```
-/// use monkey_lang::virtual_machine::bytecode::{make_instruction, OpCode};
+/// use monkey_lang::virtual_machine::bytecode::{make_bytecode, OpCode};
 /// 
-/// let result = make_instruction(OpCode::OpConstant, vec![65534]);
+/// let result = make_bytecode(OpCode::OpConstant, vec![65534]);
 /// assert_eq!(result, Ok(vec![0, 255, 254]));
 /// ```
 /// 
 /// ```
-/// use monkey_lang::virtual_machine::bytecode::{make_instruction, OpCode};
+/// use monkey_lang::virtual_machine::bytecode::{make_bytecode, OpCode};
 /// 
-/// let result = make_instruction(OpCode::OpConstant, vec![65535]);
+/// let result = make_bytecode(OpCode::OpConstant, vec![65535]);
 /// assert_eq!(result, Ok(vec![0, 255, 255]));
 /// ```
-pub fn make_instruction(opcode_name: OpCode, operands: Vec<usize>) -> Result<Instruction, String> {
+pub fn make_bytecode(opcode_name: OpCode, operands: Vec<usize>) -> Result<Instruction, String> {
     let opcode_layout = OpcodeLayout::new(opcode_name);
     let expected_operand_count = opcode_layout.operand_widths.len();
 
@@ -85,13 +85,89 @@ pub fn make_instruction(opcode_name: OpCode, operands: Vec<usize>) -> Result<Ins
     Ok(instruction) 
 }
 
+pub fn getRawAssembly(instructions: Instructions) -> Result<Vec<Byte>, String> {
+    let mut bytecode = Vec::new();
+    for instruction in instructions {
+        bytecode.append(&mut instruction.clone());
+    }
+    Ok(bytecode)
+}
+
+pub fn formatRawAssembly(bytecode: Vec<Byte>) -> Result<String, String> {
+    let mut result = String::new();
+    for instruction in bytecode {
+        result = result + &mut format!("{:?}", instruction) + "\n";
+    }
+    Ok(result)
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_make_instruction() {
+    fn test_format_raw_assembly() {
+        struct Test {
+            bytecode: Vec<Byte>,
+            expected: String,
+        }
+
+        let inputs = vec![
+            Test {
+                bytecode: vec![
+                    0, 0, 1,
+                    0, 0, 2,
+                    0, 255, 255,
+                ],
+                expected: String::from("0\n0\n1\n0\n0\n2\n0\n255\n255\n"),
+            },
+        ];
+
+        for test in inputs {
+            let result = match formatRawAssembly(test.bytecode) {
+                Ok(result) => result,
+                Err(error) => panic!("Error: {}", error),
+            };
+
+            assert_eq!(result, test.expected);
+        }
+    }
+
+    #[test]
+    fn test_make_raw_assembly() {
+        struct Test {
+            bytecode: Instructions,
+            expected: Vec<Byte>,
+        }
+
+        let inputs = vec![
+            Test {
+                bytecode: vec![
+                    make_bytecode(OpCode::OpConstant, vec![1]).unwrap(),
+                    make_bytecode(OpCode::OpConstant, vec![2]).unwrap(),
+                    make_bytecode(OpCode::OpConstant, vec![65535]).unwrap(),
+
+                ],
+                expected: vec![
+                    0, 0, 1,
+                    0, 0, 2,
+                    0, 255, 255,
+                ],
+            },
+        ];
+
+        for test in inputs {
+            let result = match getRawAssembly(test.bytecode) {
+                Ok(result) => result,
+                Err(error) => panic!("Error: {}", error),
+            };
+
+            assert_eq!(result, test.expected);
+        }
+    }
+
+    #[test]
+    fn test_make_bytecode() {
         struct Test {
             opcode: OpCode,
             operands: Vec<usize>,
@@ -114,10 +190,20 @@ mod tests {
                 operands: vec![45],
                 expected: vec![0, 0, 45],
             },
+            Test {
+                opcode: OpCode::OpConstant,
+                operands: vec![0],
+                expected: vec![0, 0, 0],
+            },
+            Test {
+                opcode: OpCode::OpConstant,
+                operands: vec![1],
+                expected: vec![0, 0, 1],
+            }
         ];
 
         for test in inputs {
-            let result = match make_instruction(test.opcode, test.operands) {
+            let result = match make_bytecode(test.opcode, test.operands) {
                 Ok(instruction) => instruction,
                 Err(error) => panic!("Error: {}", error),
             };
