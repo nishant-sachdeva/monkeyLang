73d00c1af34e7421e61af03efeb28af3893e54de
nishant-sachdeva <nishant.sachdeva@research.iiit.ac.in>
2023-06-24 21:35:41 +0530

    Added opcode, compiler support for if then else

--- src/compiler/compiler.rs
+++ src/compiler/compiler.rs
@@ -113,8 +113,31 @@ impl Compiler {
                     self.remove_last_pop();
                 }
 
-                let after_consequence_position = self.raw_assembly.instructions.len()/2;
-                _ = self.change_operand(jump_not_truthy_position, after_consequence_position)
+                if if_expr.alternative.is_some() {
+                    let jump_position = match self.emit(OpCode::OpJump, vec![9999]) {
+                        Ok(pos) => pos,
+                        Err(e) => return Err(e),
+                    };
+
+                    let after_consequence_position = self.raw_assembly.instructions.len()/2;
+                    _ = self.change_operand(jump_not_truthy_position, after_consequence_position);
+
+                    let _ = match self.compile_block(if_expr.alternative.as_ref().unwrap()) {
+                        Ok(_) => (),
+                        Err(e) => return Err(e),
+                    };
+
+                    if self.last_instruction.opcode == Some(OpCode::OpPop) {
+                        self.remove_last_pop();
+                    }
+
+                    let after_alternative_position = self.raw_assembly.instructions.len()/2;
+                    _ = self.change_operand(jump_position, after_alternative_position);
+                } else {
+                    let after_consequence_position = self.raw_assembly.instructions.len()/2;
+                    _ = self.change_operand(jump_not_truthy_position, after_consequence_position);
+                }
+                
             }
             ast::Expression::PrefixExpression(prefix) => {
                 let _ = match self.compile_expression(&prefix.right) {
@@ -543,6 +566,24 @@ mod test {
                     make_bytecode(OpCode::OpConstant, vec![1]).unwrap(),
                     make_bytecode(OpCode::OpPop, vec![]).unwrap(),
                 ]
+            },
+            CompilerTest {
+                input: String::from("if (true) { 10 } else { 20 }; 3333;"),
+                expected_constants: vec![
+                    Object::Integer(Integer {value: 10}),
+                    Object::Integer(Integer {value: 20}),
+                    Object::Integer(Integer {value: 3333})
+                ],
+                expected_instructions: vec![
+                    make_bytecode(OpCode::OpTrue, vec![]).unwrap(),
+                    make_bytecode(OpCode::OpJumpNotTruthy, vec![10]).unwrap(),
+                    make_bytecode(OpCode::OpConstant, vec![0]).unwrap(),
+                    make_bytecode(OpCode::OpJump, vec![13]).unwrap(),
+                    make_bytecode(OpCode::OpConstant, vec![1]).unwrap(),
+                    make_bytecode(OpCode::OpPop, vec![]).unwrap(),
+                    make_bytecode(OpCode::OpConstant, vec![2]).unwrap(),
+                    make_bytecode(OpCode::OpPop, vec![]).unwrap(),
+                ],
             }
         ];
         run_compiler_tests(inputs);
